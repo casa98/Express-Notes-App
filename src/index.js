7a59dfa4e448192c400b46d4f229561a1e3bd850
andressaggudelo <andresagudelo9239@gmail.com>
2020-06-19 01:01:41 -0500

    Add Settings and Middlewares to app entry point

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
+const session = require('express-sessions');
 
 // SETTINGS
 app.set('port', process.env.PORT || 3000);  // If a default port (cloud services maybe), use it, else, 3000
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views', 'layouts')),
+    partialsDir: path.join(app.get('views', 'partials')),
+    extname: '.hbs'
+    })
+);
+app.set('view engine', '.hbs');
+
 
 // MIDDLEWARES
+app.use(express.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
+app.use(session({
+    seccret: 'mysecretapp',
+    resave: true,
+    saveUninitialized: true
+}));
+
 
 // GLOBAL VARIABLES
 
