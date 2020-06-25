2ad013ef9491b4040add6bc3694e46169bb57c6d
andressaggudelo <andresagudelo9239@gmail.com>
2020-06-25 18:13:56 -0500

    Create SignUp view and send data to server

--- src/routes/user.js
+++ src/routes/user.js
@@ -9,5 +9,24 @@ router.get('/users/signup', (req, res)=>{
     res.render('users/signup');
 });
 
+router.post('/users/signup', (req, res)=>{
+    const {name, email, password, confirm_password} = req.body;
+    const errors = [];
+    if(name.length == 0){
+        errors.push({text: 'Please, insert a name'});
+    }
+    if(password != confirm_password){
+        errors.push({text: 'Passwords don\'t match'});
+    }
+    if(password.length < 6){
+        errors.push({text: 'Password must be at least 8 chararters'});
+    }
+    if(errors.length > 0){
+        // Render view again, but show error messages
+        res.render('users/signup', {errors, name, email});
+    }else{
+        res.send('Okay!');
+    }
+});
 
 module.exports = router;
